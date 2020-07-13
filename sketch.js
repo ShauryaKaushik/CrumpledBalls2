335b7ce1b9d071fef47ca20a8012e85719c9d459
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-07-13 21:06:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ function setup(){
 }
 
 function draw(){
-    background(0,0,255);
+    background(255);
     Engine.update(engine);
     rectMode(CENTER);
     rect(ground.position.x,ground.position.y,1300,20);
